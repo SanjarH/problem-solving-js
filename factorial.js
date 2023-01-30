50d1c1e5f2b1c37fab82907f9d1ada18cba2d03a
SanjarH <sanjarhoshimi@gmail.com>
2023-01-30 19:02:47 +0100

    added changes

--- factorial.js
+++ factorial.js
@@ -1,16 +1,19 @@
-/* 2. Write a function that calculates the factorial of a number 
+/* Write a function that calculates the factorial of a number 
 (the product of all positive integers less than or equal to the number). 
 For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120 */
 
-function getFactorial(input) {
+function getFactorial(input) {    
+    if (input < 0) {
+        return 0;
+    }
     var num = 1;
     var output = 1;
-    
     while(num <= input) {
         output *= num;
-        num++; // ++ syntax is used to increment the loop counter num by 1 after each iteration of the loop. 
+        num++; /* ++ syntax is used to increment the loop 
+        counter num by 1 after each iteration of the loop.*/   
     }
     return output;
 }
-console.log(getFactorial(5))
-console.log(getFactorial(0))
+console.log(getFactorial(5)) // output -> 120
+console.log(getFactorial(-5)) // output -> 0
